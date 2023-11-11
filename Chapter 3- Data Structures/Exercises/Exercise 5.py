752db958ad104be9bd4046d1dd557691be0de095
SathwikShibu <sathwik05shibu@gmail.com>
2023-11-12 03:18:50 +0400

    Update Exercise 5.py

--- Chapter 3- Data Structures/Exercises/Exercise 5.py
+++ Chapter 3- Data Structures/Exercises/Exercise 5.py
@@ -15,7 +15,7 @@ print("\nSorry, " + name + " can't make it to dinner.")
 
 # Johan can't make it! Let's invite Alan instead.
 del(guests[1])
-guests.insert(1, 'alia')
+guests.insert(1, 'sui ')
 
 # Print the invitations again.
 name = guests[0].title()
