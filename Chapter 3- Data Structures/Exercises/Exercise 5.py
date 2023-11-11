b8f43077726d34c2867e1af83f6d836682b8f4ee
SathwikShibu <sathwik05shibu@gmail.com>
2023-11-12 02:25:39 +0400

    Exercise 5.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise 5.py
@@ -0,0 +1,28 @@
+# Invite some people to dinner.
+guests = ['sabah', 'alan', 'johan']
+
+name = guests[0].title()
+print(name + ", please come to dinner.")
+
+name = guests[1].title()
+print(name + ", please come to dinner.")
+
+name = guests[2].title()
+print(name + ", please come to dinner.")
+
+name = guests[1].title()
+print("\nSorry, " + name + " can't make it to dinner.")
+
+# Johan can't make it! Let's invite Alan instead.
+del(guests[1])
+guests.insert(1, 'alia')
+
+# Print the invitations again.
+name = guests[0].title()
+print("\n" + name + ", please come to dinner.")
+
+name = guests[1].title()
+print(name + ", please come to dinner.")
+
+name = guests[2].title()
+print(name + ", please come to dinner.")
