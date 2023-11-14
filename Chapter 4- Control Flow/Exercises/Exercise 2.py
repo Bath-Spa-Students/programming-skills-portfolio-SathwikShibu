ed331c228c3cfb758256dd5b143832fd815e0411
SathwikShibu <sathwik05shibu@gmail.com>
2023-11-15 02:00:35 +0400

    Update Exercise 2.py

--- Chapter 4- Control Flow/Exercises/Exercise 2.py
+++ Chapter 4- Control Flow/Exercises/Exercise 2.py
@@ -1,7 +1,7 @@
 #Exercise 2: Alien Colors #2 :ballot_box_with_check:
 '''Choose a color for an alien as you did in Exercise 5-3, and write an if-else chain.
-•If the alien's color is green, print a statement that the player just earned 5 points for shooting the alien.
-•If the alien's color isn't green, print a statement that the player just earned 10 points.
+•If the alien's color is pink , print a statement that the player just earned 10 points for shooting the alien.
+•If the alien's color isn't purple , print a statement that the player just earned 15 points.
 •Write one version of this program that runs the if block and another that runs the else block.'''
 
 #if block
@@ -9,11 +9,11 @@ alien_color = "Pink"
 if alien_color == "Pink":
     print("You have earned 10 points for shooting the alien!")
 else:
-    print("You have earned 15 points.")
+    print("You have earned 10 points.")
 
 #else block
 alien_color = "Purple"
 if alien_color == "Purple":
-    print("You have earned 12 points for shooting the alien!")
+    print("You have earned 15 points for shooting the alien!")
 else:
-    print("You have earned 16 points.")
+    print("You have earned 15 points.")
