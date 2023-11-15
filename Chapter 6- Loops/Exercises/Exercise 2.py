a3722710daf5502989028d8b3ab797bef764c582
SathwikShibu <sathwik05shibu@gmail.com>
2023-11-15 23:55:54 +0400

    Exercise 2.py

--- Chapter 6- Loops/Exercises/Exercise 2.py
+++ Chapter 6- Loops/Exercises/Exercise 2.py
@@ -1,5 +1,5 @@
 
-'''A movie theater charges different ticket prices depending on a person's age. If a person is under the age of 3, the ticket is free; if
+'''A movie theater charges different ticket prices depending on a person’s age. If a person is under the age of 3, the ticket is free; if
 
 they are between 3 and 12, the ticket is $10; and if they are over age 12, the ticket is $15. Write a loop in which you ask users their 
 
@@ -14,7 +14,7 @@ while True:
         break
     age = int(age)
 
-    if age < 3:
+    if age < 10:
         print("  You get in free!")
     elif age < 13:
         print("  Your ticket is $10.")
